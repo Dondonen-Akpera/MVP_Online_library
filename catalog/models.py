8583454dae393fe12517906cf35fbc20fa3ce593
Dondonen Akpera <33802041+Dondonen-Akpera@users.noreply.github.com>
2023-04-19 13:53:24 +0100

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -40,7 +40,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     
-    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Foreign Key used because book can only have one language, but languages can have multiple books
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     class Meta:
